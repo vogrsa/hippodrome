73838d3a0977d66f702fe1e54ba567f637c391d2
vogrsa <rsacoding25@gmail.com>
2025-06-15 18:58:23 +0300

    add MainTest

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+public class MainTest {
+
+    @Disabled
+    @Test
+    @Timeout(value = 22)
+public void timeout() throws Exception {
+    Main.main(null);
+}
+}
